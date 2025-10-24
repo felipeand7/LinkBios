b04675b24d6e17948e73b9ad7a703990e5221207
felipeand7 <felipebaiano04@gmail.com>
2025-10-24 03:24:27 -0300

    add imagem

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ function toggleMode() {
   // substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/perfis/perfil-escuro.png");
+    // img.setAttribute("src", "./assets/perfis/perfil-escuro.png");
   } else {
     // set tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/perfis/perfil-claro.png");
+    //img.setAttribute("src", "./assets/perfis/perfil-claro.png");
   }
 }
